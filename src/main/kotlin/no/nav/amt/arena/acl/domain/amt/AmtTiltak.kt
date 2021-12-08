8715b247a6329eafcfbb637fddc1d99eb3c46f56
Magnus Evensberget <magnus.evensberget@nav.no>
2021-12-08 17:51:29 +0100

    Rydder vekk ubrukt kode

--- src/main/kotlin/no/nav/amt/arena/acl/domain/amt/AmtTiltak.kt
+++ src/main/kotlin/no/nav/amt/arena/acl/domain/amt/AmtTiltak.kt
@@ -1,25 +1,9 @@
 package no.nav.amt.arena.acl.domain.amt
 
-import no.nav.amt.arena.acl.domain.arena.ArenaTiltak
 import java.util.*
 
 data class AmtTiltak(
 	val id: UUID,
 	val kode: String,
 	val navn: String
-) : AmtPayload {
-}
-
-class AmtTiltakMapper {
-
-	companion object {
-		fun map(arenaTiltak: ArenaTiltak): AmtTiltak {
-			return AmtTiltak(
-				id = UUID.randomUUID(),
-				kode = arenaTiltak.TILTAKSKODE,
-				navn = arenaTiltak.TILTAKSNAVN
-			)
-		}
-	}
-
-}
+) : AmtPayload
