3477d965492b0758e22697e6fcb50c5bb84d3808
Magnus Evensberget <magnus.evensberget@nav.no>
2022-02-09 10:20:19 +0100

    Få testene til å kjøre sammen

--- src/test/kotlin/no/nav/amt/arena/acl/integration/IntegrationTestBase.kt
+++ src/test/kotlin/no/nav/amt/arena/acl/integration/IntegrationTestBase.kt
@@ -1,5 +1,7 @@
 package no.nav.amt.arena.acl.integration
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import no.nav.amt.arena.acl.database.SingletonPostgresContainer
 import no.nav.amt.arena.acl.integration.kafka.SingletonKafkaProvider
 import no.nav.amt.arena.acl.integration.utils.IntegrationTestTiltakUtils
@@ -9,6 +11,7 @@ import no.nav.amt.arena.acl.repositories.ArenaDataRepository
 import no.nav.amt.arena.acl.repositories.TiltakRepository
 import no.nav.common.kafka.producer.KafkaProducerClientImpl
 import org.junit.runner.RunWith
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
@@ -17,6 +20,8 @@ import org.springframework.context.annotation.Import
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
 import javax.sql.DataSource
 
 @RunWith(SpringRunner::class)
@@ -70,14 +75,58 @@ abstract class IntegrationTestBase {
 
 @TestConfiguration
 open class IntegrationTestConfiguration {
+	private val log = LoggerFactory.getLogger(this::class.java)
+	private val postgresDockerImageName = "postgres:12-alpine"
 
 	@Bean
 	open fun dataSource(): DataSource {
-		return SingletonPostgresContainer.getDataSource()
+		return getDataSource()
 	}
 
 	@Bean
 	open fun kafkaProperties(): KafkaProperties {
 		return SingletonKafkaProvider.getKafkaProperties()
+	}
+
+	private var postgresContainer: PostgreSQLContainer<Nothing>? = null
+
+	private var containerDataSource: DataSource? = null
+
+	fun getDataSource(): DataSource {
+		if (containerDataSource == null) {
+			containerDataSource = createDataSource(getContainer())
+		}
+
+		return containerDataSource!!
+	}
+
+	private fun getContainer(): PostgreSQLContainer<Nothing> {
+		if (postgresContainer == null) {
+			log.info("Starting new postgres database...")
+
+			val container = createContainer()
+			postgresContainer = container
+
+			container.start()
+
+		}
+
+		return postgresContainer as PostgreSQLContainer<Nothing>
+	}
+
+	private fun createContainer(): PostgreSQLContainer<Nothing> {
+		return PostgreSQLContainer(DockerImageName.parse(postgresDockerImageName))
+	}
+
+	private fun createDataSource(container: PostgreSQLContainer<Nothing>): DataSource {
+		val config = HikariConfig()
+
+		config.jdbcUrl = container.jdbcUrl
+		config.username = container.username
+		config.password = container.password
+		config.maximumPoolSize = 3
+		config.minimumIdle = 1
+
+		return HikariDataSource(config)
 	}
 }
