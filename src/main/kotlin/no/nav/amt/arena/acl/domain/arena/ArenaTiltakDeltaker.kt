ea2453f4a53042757ff803b86bf1f60ff364e556
Alexander Gård <alex.gaard@hotmail.com>
2022-02-18 09:14:04 +0100

    Nullable MOD_USER

--- src/main/kotlin/no/nav/amt/arena/acl/domain/arena/ArenaTiltakDeltaker.kt
+++ src/main/kotlin/no/nav/amt/arena/acl/domain/arena/ArenaTiltakDeltaker.kt
@@ -15,7 +15,7 @@ data class ArenaTiltakDeltaker(
 	val REG_DATO: String?,
 	val REG_USER: String?,
 	val MOD_DATO: String,
-	val MOD_USER: String,
+	val MOD_USER: String?,
 	val DATO_SVARFRIST: String?,
 	val DATO_FRA: String?,
 	val DATO_TIL: String?,
