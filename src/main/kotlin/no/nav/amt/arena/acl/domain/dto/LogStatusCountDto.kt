d6b08e3f09108a9ae24d0e84c33accd0d822ce54
izisfro <izis.froyhov@nav.no>
2022-03-04 09:47:25 +0100

    Forenkler og fikser metrikk som ikke fungerte

--- src/main/kotlin/no/nav/amt/arena/acl/domain/dto/LogStatusCountDto.kt
+++ src/main/kotlin/no/nav/amt/arena/acl/domain/dto/LogStatusCountDto.kt
@@ -3,7 +3,6 @@ package no.nav.amt.arena.acl.domain.dto
 import no.nav.amt.arena.acl.domain.IngestStatus
 
 data class LogStatusCountDto(
-	val table: String,
 	val status: IngestStatus,
 	val count: Int
 )
