4cc6e7507da7d7221a3f3e7837816fceeb28f0a9
Izis Frøyhov <izis.froyhov@nav.no>
2023-01-12 11:29:52 +0100

    quick fix for problem med tester
    Co-authored-by: Tormod Fossum <tormod.fossum@nav.no>

--- src/test/kotlin/no/nav/amt/arena/acl/repositories/GjennomforingRepositoryTest.kt
+++ src/test/kotlin/no/nav/amt/arena/acl/repositories/GjennomforingRepositoryTest.kt
@@ -1,43 +1,36 @@
 package no.nav.amt.arena.acl.repositories
 
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import no.nav.amt.arena.acl.database.DatabaseTestUtils
 import no.nav.amt.arena.acl.database.SingletonPostgresContainer
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
-class GjennomforingRepositoryTest {
+class GjennomforingRepositoryTest : FunSpec({
 	val dataSource = SingletonPostgresContainer.getDataSource()
-	val repository: GjennomforingRepository = GjennomforingRepository(NamedParameterJdbcTemplate(dataSource))
+	lateinit var repository: GjennomforingRepository
 
-	@BeforeEach
-	fun before() {
-		DatabaseTestUtils.cleanDatabase(dataSource)
+	beforeEach {
+		repository = GjennomforingRepository(NamedParameterJdbcTemplate(dataSource))
 	}
 
-	@Test
-	fun `upsert - skal upserte gjennomforing`() {
+	test("upsert - skal upserte gjennomforing"){
 		val arenaId = "ARENA_ID"
 		repository.upsert(arenaId,"INDOPPFAG", true)
 
 		repository.isValid(arenaId) shouldBe true
 	}
 
-	@Test
-	fun `upsert - skal oppdatere eksisterende`() {
-		val arenaId = "ARENA_ID"
+	test("upsert - skal oppdatere eksisterende") {
+		val arenaId = "ARENA_ID2"
 		repository.upsert(arenaId,"INDOPPFAG", false)
 		repository.upsert(arenaId,"INDOPPFAG", true)
 
-
 		repository.isValid(arenaId) shouldBe true
 	}
 
-	@Test
-	fun `isValid - skal hente riktig record - flere er tilgjengelig`() {
-		val arenaId1 = "ARENA_ID1"
-		val arenaId2 = "ARENA_ID2"
+	test("isValid - skal hente riktig record - flere er tilgjengelig") {
+		val arenaId1 = "ARENA_ID3"
+		val arenaId2 = "ARENA_ID4"
 		repository.upsert(arenaId1,"INDOPPFAG", true)
 		repository.upsert(arenaId2,"INDOPPFAG", false)
 
@@ -45,4 +38,4 @@ class GjennomforingRepositoryTest {
 		repository.isValid(arenaId2) shouldBe false
 
 	}
-}
+})
