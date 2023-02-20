ae78f600aae11158a41e5c8ce14b8b5866aac6e0
Izis Frøyhov <izis.froyhov@nav.no>
2023-02-20 16:12:30 +0100

    Fjerner unique fra index

--- src/main/resources/db/migration/V12__index-arena-data.sql
+++ src/main/resources/db/migration/V12__index-arena-data.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX arena_data_table_name_arena_id_idx on arena_data (arena_id, arena_table_name);
+CREATE INDEX arena_data_table_name_arena_id_idx on arena_data (arena_id, arena_table_name);
