9e41da81caf38e3e8eed81d6efe6c2b759b5d9eb
Tia Firing <tia.firing@nav.no>
2024-08-30 11:01:01 +0200

    må sjekke om brukeren finnes først

--- src/main/resources/db/migration/V15__grants-new-table.sql
+++ src/main/resources/db/migration/V15__grants-new-table.sql
@@ -1,1 +1,9 @@
-grant all on all tables in schema public to cloudsqliamuser;
+DO
+$$
+BEGIN
+    IF (SELECT exists(SELECT rolname FROM pg_roles WHERE rolname = 'cloudsqliamuser'))
+    THEN
+        grant all on all tables in schema public to cloudsqliamuser;
+    END IF;
+END
+$$;
