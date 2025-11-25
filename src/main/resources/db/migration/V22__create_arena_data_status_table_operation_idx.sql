5b8e4a6698cb1a745e6a666d83312a75391709f5
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-11-25 22:25:13 +0100

    Revert "Forsøker med med ny versjon av migreringen for at deploy til dev skal passere"
    This reverts commit 052cfca0e5fbe6ec56ed83cd0a65d429334f50b2.

--- src/main/resources/db/migration/V22__create_arena_data_status_table_operation_idx.sql
+++ src/main/resources/db/migration/V22__create_arena_data_status_table_operation_idx.sql
@@ -1,13 +1,2 @@
-DO $$
-    BEGIN
-        IF NOT EXISTS (
-            SELECT 1
-            FROM pg_class c
-                     JOIN pg_namespace n ON n.oid = c.relnamespace
-            WHERE c.relname = 'arena_data_status_table_operation_idx'
-              AND c.relkind = 'i'
-        ) THEN
-            EXECUTE 'CREATE INDEX CONCURRENTLY arena_data_status_table_operation_idx
-                ON arena_data (ingest_status, arena_table_name, operation_pos)';
-        END IF;
-    END$$;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS arena_data_status_table_operation_idx
+    ON arena_data (ingest_status, arena_table_name, operation_pos);
