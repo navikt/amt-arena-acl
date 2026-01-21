6ca0e55f85103004f1a85a8c6a47c79cbefe5dbd
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-01-21 13:27:40 +0100

    disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)

--- src/main/kotlin/no/nav/amt/arena/acl/utils/JsonUtils.kt
+++ src/main/kotlin/no/nav/amt/arena/acl/utils/JsonUtils.kt
@@ -1,8 +1,12 @@
 package no.nav.amt.arena.acl.utils
 
+import tools.jackson.databind.DeserializationFeature
 import tools.jackson.databind.ObjectMapper
-import tools.jackson.module.kotlin.jacksonObjectMapper
+import tools.jackson.module.kotlin.jacksonMapperBuilder
 
 object JsonUtils {
-	val objectMapper: ObjectMapper = jacksonObjectMapper()
+	val objectMapper: ObjectMapper =
+		jacksonMapperBuilder()
+			.disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+			.build()
 }
