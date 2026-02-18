31e045024cb4e6878929aa51de5863d46546386a
Izis Frøyhov <izis.froyhov@nav.no>
2026-02-18 08:44:58 +0100

    Lager lokal kopi av konvertering til arenakode sånn at vi ikke leser egne data på tiltak etter ny forskrift

--- src/main/kotlin/no/nav/amt/arena/acl/clients/mulighetsrommet/MulighetsrommetApiClient.kt
+++ src/main/kotlin/no/nav/amt/arena/acl/clients/mulighetsrommet/MulighetsrommetApiClient.kt
@@ -2,12 +2,26 @@ package no.nav.amt.arena.acl.clients.mulighetsrommet
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import no.nav.amt.arena.acl.utils.JsonUtils.objectMapper
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.ArenaKode
 import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ARBEIDSFORBEREDENDE_TRENING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ARBEIDSRETTET_REHABILITERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.AVKLARING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.DIGITALT_OPPFOLGINGSTILTAK
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.ENKELTPLASS_FAG_OG_YRKESOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.GRUPPE_ARBEIDSMARKEDSOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.GRUPPE_FAG_OG_YRKESOPPLAERING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.HOYERE_UTDANNING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.JOBBKLUBB
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.OPPFOLGING
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode.VARIG_TILRETTELAGT_ARBEID_SKJERMET
 import no.nav.common.rest.client.RestClient.baseClient
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.springframework.http.HttpHeaders
 import tools.jackson.module.kotlin.readValue
+import java.lang.IllegalArgumentException
 import java.util.UUID
 import java.util.function.Supplier
 
@@ -108,7 +122,7 @@ class MulighetsrommetApiClient(
 			val arenaKode =
 				Tiltakskode
 					.valueOf(effectiveTiltakskode)
-					.toArenaKode()
+					.toArenaKodeLocal()
 					.name
 
 			return Gjennomforing(
@@ -118,4 +132,25 @@ class MulighetsrommetApiClient(
 			)
 		}
 	}
+}
+
+fun Tiltakskode.toArenaKodeLocal() = when (this) {
+	ARBEIDSFORBEREDENDE_TRENING -> ArenaKode.ARBFORB
+	ARBEIDSRETTET_REHABILITERING -> ArenaKode.ARBRRHDAG
+	AVKLARING -> ArenaKode.AVKLARAG
+	DIGITALT_OPPFOLGINGSTILTAK -> ArenaKode.DIGIOPPARB
+	GRUPPE_ARBEIDSMARKEDSOPPLAERING -> ArenaKode.GRUPPEAMO
+	GRUPPE_FAG_OG_YRKESOPPLAERING -> ArenaKode.GRUFAGYRKE
+	JOBBKLUBB -> ArenaKode.JOBBK
+	OPPFOLGING -> ArenaKode.INDOPPFAG
+	VARIG_TILRETTELAGT_ARBEID_SKJERMET -> ArenaKode.VASV
+	ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING -> ArenaKode.ENKELAMO
+	ENKELTPLASS_FAG_OG_YRKESOPPLAERING -> ArenaKode.ENKFAGYRKE
+	HOYERE_UTDANNING -> ArenaKode.HOYEREUTD
+	Tiltakskode.ARBEIDSMARKEDSOPPLAERING -> ArenaKode.GRUPPEAMO
+	Tiltakskode.NORSKOPPLAERING_GRUNNLEGGENDE_FERDIGHETER_FOV -> ArenaKode.GRUPPEAMO
+	Tiltakskode.STUDIESPESIALISERING -> ArenaKode.GRUPPEAMO
+	Tiltakskode.FAG_OG_YRKESOPPLAERING -> ArenaKode.GRUFAGYRKE
+	Tiltakskode.HOYERE_YRKESFAGLIG_UTDANNING -> ArenaKode.GRUFAGYRKE
+	else -> throw IllegalArgumentException("Ukjent tiltakskode: $this")
 }
