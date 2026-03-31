86061fd59fed2a685b11c68935b144a8e7faed0a
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-03-31 07:17:59 +0200

    kotlinVersion = "2.3.10"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    val kotlinVersion = "2.3.20"
+    val kotlinVersion = "2.3.10"
     val springBootVersion = "4.0.4"
     val springDependencyManagementVersion = "1.1.7"
 
